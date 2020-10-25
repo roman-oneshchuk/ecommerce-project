2a42546a5e5b3f16ce95490b0b0cd2757489c48c
Roman <roman.oneshchuk@gmail.com>
2020-10-26 01:55:08 +0200

    product page active

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -12,8 +13,14 @@ class Category(models.Model):
 		verbose_name = 'category'
 		verbose_name_plural = 'categories'
 
+
+	def get_url(self):
+		return reverse('products_by_category', args=[self.slug])
+
+
 	def __str__(self): #чтобы возвращать удобочитаемую репрезентацию модели.
 		return self.name
+
 
 
 class Product(models.Model):
@@ -32,6 +39,11 @@ class Product(models.Model):
 		ordering = ('name', )
 		verbose_name = 'product'
 		verbose_name_plural = 'products'
+
+
+	def get_url(self):
+		return reverse('product_detail', args=[self.category.slug, self.slug])
+			
 
 	def __str__(self):
 		return self.name
--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/', views.product, name='product'),
+
+    path('<slug:category_slug>', views.home, name='products_by_category'),
+
+    path('<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
 ]
