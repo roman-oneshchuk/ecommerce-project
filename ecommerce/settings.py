da6820bc245b877763474f6aec2c374cc9be745f
Roman <roman.oneshchuk@gmail.com>
2020-10-27 21:35:43 +0200

    crispy form added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'static')
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
